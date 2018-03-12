bd61e5d4faa94eb8b5da56f4b095d3b61d6f5f85
david-daly <david-daly@live.ie>
2018-03-12 23:04:47 +0000

    Update style to resemble table (needs to be fixed)

--- src/components/Ticker.vue
+++ src/components/Ticker.vue
@@ -1,12 +1,20 @@
 <template>
     <main>
-       <p v-for="coin of this.ticker.coins" :key="coin.id">
-         {{coin.symbol}}<br>
-         €{{coin.price_eur}}<br>
-         €{{coin.boughtFor}}<br>
-         €{{coin.currentAmount}}<br>
-         €{{coin.profit}}<br>
-       </p>
+      <div class="currencies">
+        <div>Symbol</div>
+        <div>Price</div>
+        <div>Bought</div>
+        <div>Current Amount</div>
+        <div>Profit</div>
+      </div>
+
+      <div class="currencies" v-for="coin of this.ticker.coins" :key="coin.id">
+        <div>{{coin.symbol}}</div>
+        <div>€{{coin.price_eur}}</div>
+        <div>€{{coin.boughtFor}}</div>
+        <div>€{{coin.currentAmount}}</div>
+        <div>€{{coin.profit}}</div>
+      </div>
     </main>
 </template>
 
@@ -34,11 +42,19 @@ main {
     background-repeat: no-repeat;
 
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: space-around;
     align-items: center;
 
     box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+}
+
+.currencies {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
 }
 
 </style>
